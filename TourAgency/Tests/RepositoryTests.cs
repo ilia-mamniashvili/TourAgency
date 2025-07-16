479ce04e6496ab84527170c917fe77eeec81b743
ilia-mamniashvili <iliamamniashvili02@gmail.com>
2025-07-17 03:23:11 +0400

    UnitOfWork Dispose

---
+++ TourAgency/Tests/RepositoryTests.cs
@@ -0,0 +1,14 @@
+namespace Tests;
+public class RepositoryTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
