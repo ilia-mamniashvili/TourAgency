167b6c7c5baf0c85105b5c6c27ed62ffaec42cfd
ilia-mamniashvili <iliamamniashvili02@gmail.com>
2025-07-16 18:38:44 +0400

    Refactor UnitOfWork for explicit repos and lazy init

--- TourAgency/Repositories/Interfaces/IUnitOfWork.cs
+++ TourAgency/Repositories/Interfaces/IUnitOfWork.cs
@@ -8,12 +8,20 @@ namespace Repositories.Interfaces
 {
     public interface IUnitOfWork : IDisposable
     {
-        IRepository<T> Repository<T>() where T : class;
-
         Task<int> SaveChangesAsync();
         Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess);
         Task RollbackAsync();
         Task CommitAsync();
+
+        ICityRepository City { get; }
+        ICountryRepository Country { get; }
+        IEntityStatusRepository EntityStatus { get; }
+        IHotelRepository Hotel { get; }
+        IServiceRepository Service { get; }
+        ITourBookingRepository TourBooking { get; }
+        ITouristRepository Tourist { get; }
+        ITourItemRepository TourItem { get; }
+        ITourRepository Tour { get; }
 
     }
 }
--- TourAgency/Repositories/UnitOfWork.cs
+++ TourAgency/Repositories/UnitOfWork.cs
@@ -2,24 +2,51 @@
 using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.DependencyInjection;
 using Repositories.Interfaces;
+using System.Data;
 
 namespace Repositories
 {
     public class UnitOfWork : IUnitOfWork
     {
         private readonly TourAgencyDbContext _context;
-        private readonly IServiceProvider _serviceProvider;
         private IDbContextTransaction? _transaction;
+        private bool _disposed;
 
-        public UnitOfWork(TourAgencyDbContext context, IServiceProvider serviceProvider)
+        //Lazy ლოგიკის იმპლემენტაცია
+        private Lazy<ICityRepository> _city;
+        private Lazy<ICountryRepository> _country;
+        private Lazy<IEntityStatusRepository> _entityStatus;
+        private Lazy<IHotelRepository> _hotel;
+        private Lazy<IServiceRepository> _service;
+        private Lazy<ITourBookingRepository> _tourBooking;
+        private Lazy<ITouristRepository> _tourist;
+        private Lazy<ITourItemRepository> _tourItem;
+        private Lazy<ITourRepository> _tour;
+
+        public ICityRepository City => _city.Value;
+        public ICountryRepository Country => _country.Value;
+        public IEntityStatusRepository EntityStatus => _entityStatus.Value;
+        public IHotelRepository Hotel => _hotel.Value;
+        public IServiceRepository Service => _service.Value;
+        public ITourBookingRepository TourBooking => _tourBooking.Value;
+        public ITouristRepository Tourist => _tourist.Value;
+        public ITourItemRepository TourItem => _tourItem.Value;
+        public ITourRepository Tour => _tour.Value;
+
+        public UnitOfWork(TourAgencyDbContext context)
+            
         {
             _context = context;
-            _serviceProvider = serviceProvider;
-        }
-
-        public IRepository<T> Repository<T>() where T : class
-        {
-            return _serviceProvider.GetRequiredService<IRepository<T>>();
+            
+            _city = new Lazy<ICityRepository>(() => new CityRepository(_context));
+            _country = new Lazy<ICountryRepository>(() => new CountryRepository(_context));
+            _entityStatus = new Lazy<IEntityStatusRepository>(() => new EntityStatusRepository(_context));
+            _hotel = new Lazy<IHotelRepository>(() => new HotelRepository(_context));
+            _service = new Lazy<IServiceRepository>(() => new ServiceRepository(_context));
+            _tourBooking = new Lazy<ITourBookingRepository>(() => new TourBookingRepository(_context));
+            _tourist = new Lazy<ITouristRepository>(() => new TouristRepository(_context));
+            _tourItem = new Lazy<ITourItemRepository>(() => new TourItemRepository(_context));
+            _tour = new Lazy<ITourRepository>(() => new TourRepository(_context));
         }
 
         public async Task<int> SaveChangesAsync()
@@ -54,14 +81,15 @@ namespace Repositories
         {
             if (_transaction != null)
             {
-                await _transaction.RollbackAsync();
-                await _transaction.DisposeAsync();
+                 await _transaction.RollbackAsync();
+                 await _transaction.DisposeAsync();
                 _transaction = null;
             }
         }
 
         public void Dispose()
         {
+            _transaction?.Dispose();
             _context.Dispose();
         }
     }
