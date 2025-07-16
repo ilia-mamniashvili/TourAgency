6664971107f4239445a4dbeeae2ebcdefa9de2ac
ilia-mamniashvili <iliamamniashvili02@gmail.com>
2025-07-16 12:51:30 +0400

    Added IUnitOfWork and UnitOfWork

---
+++ TourAgency/Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<T> Repository<T>() where T : class;
+
+        Task<int> SaveChangesAsync();
+        Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess);
+        void Rollback();
+        void Commit();
+    
+    }
+}
--- TourAgency/Repositories/UnitOfWork.cs
+++ TourAgency/Repositories/UnitOfWork.cs
@@ -1,13 +1,48 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Repositories.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Repositories
+namespace Repositories;
+public class UnitOfWork : IUnitOfWork
 {
-    public class UnitOfWork
+    private readonly TourAgencyDbContext _context;
+    private readonly IServiceProvider _serviceProvider;
+
+    //Constructor 
+    public UnitOfWork(TourAgencyDbContext context, IServiceProvider serviceProvider)
     {
+        _context = context;
+        _serviceProvider = serviceProvider;
+    }
+
+    public IRepository<T> Repository<T>() where T : class
+    {
+        return _serviceProvider.GetRequiredService<IRepository<T>>();
+    }
+
+    public async Task<int> SaveChangesAsync()
+    {
+        return await _context.SaveChangesAsync();
+    }
+
+    public async Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess)
+    {
+        return await _context.SaveChangesAsync(acceptAllChangesOnSuccess);
+    }
+
+    public void Commit() { }
+
+    public void Rollback() { }
+
+    public void Dispose()
+    {
+        _context.Dispose();
     }
 }
+
 
